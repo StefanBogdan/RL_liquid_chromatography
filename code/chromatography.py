031f7baa7b872a9ab9c66e2353931a018b8d3018
Stefan Bogda <stefanbogdan180394@gmail.com>
2019-12-17 23:13:56 +0100

    Added ExperimentAnalytes class

--- code/chromatography.py
+++ code/chromatography.py
@@ -109,3 +109,150 @@ def overlap_error(mu, sigma):
     )
     
     return overlap_area/(len(mu) - 1)
+
+class ExperimentAnalytes(object):
+    """
+    Encapsulation of the Experiment data.
+    """
+    
+    
+    def __init__(self, k0, S, h=0.001, run_time=None, grad='iso'):
+        """
+        Initialize the class properties.
+        """
+        
+        
+        # 'k0' var datatype check
+        if not isinstance(k0, np.ndarray):
+            raise TypeError(f"'k0' is of a wrong data type (given '{type(k0)}', needed 'numpy.ndarray')")
+            
+        # 'S' var datatype check
+        if not isinstance(S, np.ndarray):
+            raise TypeError(f"'S' is of a wrong data type (given '{type(S)}', needed 'numpy.ndarray')")
+        
+        # 'k0' and 'S' shape match
+        if not k0.shape == S.shape:
+            raise ValueError(f"Shape mismatch: k0 -> {k0.shape}, S -> {S.shape}")
+        
+        ### Set initial values ###
+        
+        self.k0 = k0
+        self.S = S
+        self.h = float(h)
+        if run_time:
+            self.run_time = float(run_time)
+        else:
+            self.run_time = run_time
+            
+        self.time_travel = np.zeros(k0.shape)
+        self.positions = np.zeros(k0.shape)
+        self.final_position = 1/(1 + 2 * h ** 0.5)
+        
+        if grad == 'iso':
+            self.x = self.x_iso
+            self.intersection_time = self.intersection_time_iso
+            self.step = self.step_iso
+        elif grad == 'linear':
+            self.x = self.x_linear
+            self.intersection_time = self.intersection_time_linear
+            self.step = self.step_linear
+        else:
+            raise ValueError(f"'grad' can have only these values ['iso', 'linear'], given '{grad}'")
+    
+        
+    def k(self, phi):
+        """
+        Compute retention factor k a given phi.
+        """
+        
+        return (10 ** (-phi * self.S)) * self.k0
+    
+    
+    @property
+    def sig(self):
+        """
+        Compute second Gaussian moment for the analytes.
+        """
+        
+        return np.sqrt((self.position**2) * self.h)
+    
+    
+    def intersection_time_iso(self, phi, delta_tau_phi):
+        """
+        Compute time needed for the analytes to reach 
+        the new solvent for isocratic gradient function.
+        """
+        return delta_tau_phi * (1 + self.k(phi)) / self.k(phi)
+    
+    
+    def x_iso(self, phi, delta_tau):
+        """
+        Compute distance traveled by the analytes for a 
+        given solvent and time interval,
+        for isocratic gradient function.
+        """
+        
+        return delta_tau /(self.k(phi) + 1)
+    
+    
+    def step_iso(self, phi, delta_tau_phi):
+        """
+        Compute Time and Distance traveled by each analyte.
+        Update Distance and Time traveled by each analyte.
+        Isocratic gradient function.
+        """
+        # Compute the interval of time needed to intersect with the next phi.
+        intersection_tau = self.intersection_time(phi, delta_tau_phi)
+        # Compute the distance Traveled until the intersection.
+        delta_x = self.x(phi, intersection_tau)
+
+        over_x_lim = (self.positions + delta_x > self.final_position)
+        #print(f'Over X:{over_x_lim.any()}')
+        if over_x_lim.any():
+            max_time = (self.time_travel[over_x_lim] + \
+                        (self.final_position - self.positions[over_x_lim]) * \
+                        (self.k(phi)[over_x_lim] + 1)).min()
+            
+            if self.run_time:
+                self.run_time = np.min([self.run_time, max_time])
+            else:
+                self.run_time = max_time
+        #print(f'Run Time:{self.run_time}')
+        if self.run_time:
+            over_time = (self.time_travel + intersection_tau > self.run_time)
+            if over_time.any():
+                intersection_tau[over_time] = self.run_time - self.time_travel[over_time]
+        #print(f'Time:{intersection_tau}\n')
+        # Update time travel and position.
+        self.time_travel += intersection_tau
+        self.positions += self.x_iso(phi, intersection_tau)
+        
+        return self.time_travel, self.positions
+
+    
+    def intersection_time_linear(self, phi_init, phi_final, delta_tau_phi):
+        """
+        Compute time needed for the analytes to reach 
+        the new solvent for linear gradient function.
+        """
+        
+        pass
+    
+    
+    def x_linear(self, phi_init, phi_final, delta_tau):
+        """
+        Compute distance traveled by the analytes for a 
+        given solvent and time interval,
+        for linear gradient function.
+        """
+        
+        pass
+        
+
+    def step_linear(self, phi_init, phi_final, delta_tau_phi):
+        """
+        Compute Time and Distance traveled by each analyte.
+        Update Distance and Time traveled by each analyte.
+        Isocratic gradient function.
+        """
+        pass
