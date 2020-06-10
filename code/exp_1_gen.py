db34c71052fe15f4499d0c7df4ccb952422c18b8
Stefan Bogda <stefanbogdan180394@gmail.com>
2020-06-10 14:30:27 +0200

    Added experiment python files

---
+++ code/exp_1_gen.py
@@ -0,0 +1,118 @@
+import numpy as np
+import pandas as pd
+from chromatography import *
+from separation_utility import *
+from torch import optim, tensor
+import torch.nn as nn
+import matplotlib.pyplot as plt
+import time
+
+alists = []
+alists.append(pd.read_csv('../data/GilarSample.csv'))
+alists.append(pd.read_csv('../data/Alizarin.csv'))
+alists.append(pd.read_csv('../data/Peterpeptides.csv'))
+alists.append(pd.read_csv('../data/Roca.csv'))
+alists.append(pd.read_csv('../data/Peter32.csv'))
+alists.append(pd.read_csv('../data/Eosin.csv'))
+alists.append(pd.read_csv('../data/Controlmix2.csv'))
+alists.append(pd.read_csv('../data/Gooding.csv'))
+# GilarSample - 8 analytes
+# Peterpeptides - 32 analytes
+# Roca - 14 analytes
+# Peter32 - 32 analytes
+# Eosin - 20 analytes
+# Alizarin - 16 analytes
+# Controlmix2 - 17 analytes
+# Gooding - 872 analytes
+
+# Parameters
+all_analytes = pd.concat(alists, sort=True).reset_index()[['k0', 'S', 'lnk0']]
+
+kwargs = {
+    'num_episodes' : 25_000, 
+    'sample_size' : 10,
+    'batch_size' : 1, 
+    'lr' : .05, 
+    'optim' : torch.optim.SGD,
+    'lr_decay_factor' : 0.75,
+    'lr_milestones' : 5000,
+    'print_every' : 25_001,
+    'baseline' : .55,
+    'max_norm' : 1.5,
+    'max_rand_analytes' : 40,
+    'min_rand_analytes' : 8,
+    'rand_prob' : 1.,
+    'h' : 0.001,
+    'run_time' : 1.
+}
+N = 7
+M = 15
+
+losses_50_50 = np.zeros((N, M, kwargs['num_episodes']))
+test_losses_50_50 = np.zeros((N, M, kwargs['num_episodes']))
+losses_100 = np.zeros((N, M, kwargs['num_episodes']))
+
+for n in range(0, N):
+    print(n)
+    delta_taus = np.ones(n + 1) * 1/(n + 1)
+    
+    for i in range(M):
+        alist_train = all_analytes.sample(frac=0.5)
+        alist_test = all_analytes.loc[lambda a: ~a.index.isin(alist_train.index.values)]
+        print(f"  {i}")
+        #Policies
+        pol_50_50 = pol = PolicyGeneral(
+            phi = nn.Sequential(
+                PermEqui2_max(2, 5),
+                nn.ELU(inplace=True),
+                PermEqui2_max(5, 5),
+                nn.ELU(inplace=True),
+                PermEqui2_max(5, 5),
+                nn.ELU(inplace=True),
+            ),
+            rho = nn.Sequential(
+                nn.Linear(5, 5),
+                nn.ELU(inplace=True),
+                nn.Linear(5, 5),
+                nn.ELU(inplace=True),
+                Rho(n_steps=len(delta_taus), hidden=5, in_dim=5, sigma_max=.3, sigma_min=.01),
+            )
+        )
+        pol_100 = pol = PolicyGeneral(
+            phi = nn.Sequential(
+                PermEqui2_max(2, 5),
+                nn.ELU(inplace=True),
+                PermEqui2_max(5, 5),
+                nn.ELU(inplace=True),
+                PermEqui2_max(5, 5),
+                nn.ELU(inplace=True),
+            ),
+            rho = nn.Sequential(
+                nn.Linear(5, 5),
+                nn.ELU(inplace=True),
+                nn.Linear(5, 5),
+                nn.ELU(inplace=True),
+                Rho(n_steps=len(delta_taus), hidden=5, in_dim=5, sigma_max=.3, sigma_min=.01),
+            )
+        )
+        # Run Exp
+        loss, loss_test = reinforce_gen(
+            alists = [alist_train], 
+            test_alist = alist_test,
+            policy = pol_50_50, 
+            delta_taus = delta_taus, 
+            **kwargs
+        )
+        loss_100, _ = reinforce_gen(
+            alists = [all_analytes], 
+            test_alist = None,
+            policy = pol_100, 
+            delta_taus = delta_taus, 
+            **kwargs
+        )
+        
+        losses_50_50[n,i] = loss
+        test_losses_50_50[n,i] = loss_test
+        losses_100[n,i] = loss_100
+
+np.savez_compressed("../results/general_perf_vs_n_steps", losses_50_50=losses_50_50, test_losses_50_50=test_losses_50_50, losses_100=losses_100)
---
+++ code/exp_3_single.py
@@ -0,0 +1,97 @@
+import numpy as np
+import pandas as pd
+from chromatography import ExperimentAnalytes
+from separation_utility import *
+from torch import optim, tensor
+import matplotlib.pyplot as plt
+import time
+
+alists = []
+alists.append(pd.read_csv(f'../data/GilarSample.csv'))
+alists.append(pd.read_csv(f'../data/Alizarin.csv'))
+alists.append(pd.read_csv(f'../data/Peterpeptides.csv'))
+alists.append(pd.read_csv(f'../data/Roca.csv'))
+alists.append(pd.read_csv(f'../data/Peter32.csv'))
+alists.append(pd.read_csv(f'../data/Eosin.csv'))
+alists.append(pd.read_csv(f'../data/Controlmix2.csv'))
+# GilarSample - 8 analytes
+# Peterpeptides - 32 analytes
+# Roca - 14 analytes
+# Peter32 - 32 analytes
+# Eosin - 20 analytes
+# Alizarin - 16 analytes
+# Controlmix2 - 17 analytes
+# Gooding - 872 analytes
+
+
+# Parameters
+sigma_max = 0.3
+
+kwargs = {
+'num_episodes' : 6000,
+'sample_size' : 10,
+'lr' : .05,
+'optim' : torch.optim.SGD,
+'lr_decay_factor' : .5,
+'lr_milestones' : 1000,
+'print_every' : 6001,
+'baseline' : 0.55,
+'max_norm' : 2.
+}
+N = 6
+M = 50
+# Experiments
+exp_8 = ExperimentAnalytes(k0 = alists[0].k0.values, S = alists[0].S.values, h=0.001, run_time=1.0)
+exp_16 = ExperimentAnalytes(k0 = alists[1].k0.values, S = alists[1].S.values, h=0.001, run_time=1.0)
+exp_32 = ExperimentAnalytes(k0 = alists[2].k0.values, S = alists[2].S.values, h=0.001, run_time=1.0)
+# Final Results 
+dist_tau_8 = np.zeros((N, M))
+dist_tau_16 = np.zeros((N, M))
+dist_tau_32 = np.zeros((N, M))
+
+for n in range(0, N): 
+    print(f"{n}")
+    for i in range(M):
+        print(f"  {i}")
+        #Policies
+        pol_tau_8 = PolicySingleTime(n + 2, sigma_max = sigma_max)
+        pol_tau_16 = PolicySingleTime(n + 2, sigma_max = sigma_max)
+        pol_tau_32 = PolicySingleTime(n + 2, sigma_max = sigma_max)
+        # Run Exp 8
+        reinforce_delta_tau(
+                exp_8, 
+                pol_tau_8,
+                **kwargs
+            )
+        # Run Exp 16
+        reinforce_delta_tau(
+                exp_16, 
+                pol_tau_16,
+                **kwargs
+            )
+        # Run Exp 32
+        reinforce_delta_tau(
+                exp_32, 
+                pol_tau_32,
+                **kwargs
+            )
+        
+        exp_8.reset()
+        exp_16.reset()
+        exp_32.reset()
+        mu_8, _ = pol_tau_8.forward()
+        mu_16, _ = pol_tau_16.forward()
+        mu_32, _ = pol_tau_32.forward()
+        grads_8, delta_taus_8 = np.split(torch.cat((mu_8, tensor([1.])), 0).data.numpy(), 2)
+        grads_16, delta_taus_16 = np.split(torch.cat((mu_16, tensor([1.])), 0).data.numpy(), 2)
+        grads_32, delta_taus_32 = np.split(torch.cat((mu_32, tensor([1.])), 0).data.numpy(), 2)
+        exp_8.run_all(grads_8, delta_taus_8)
+        exp_16.run_all(grads_16, delta_taus_16)
+        exp_32.run_all(grads_32, delta_taus_32)
+        
+        dist_tau_8[n, i] = exp_8.loss()
+        dist_tau_16[n, i] = exp_16.loss()
+        dist_tau_32[n, i] = exp_32.loss()
+
+
+        np.savez_compressed("../results/distribution_single_delta_tau_perf_vs_n_steps", nr_analytes_8=dist_tau_8, nr_analytes_16=dist_tau_16, nr_analytes_32=dist_tau_32)
